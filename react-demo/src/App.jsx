678fd0c0b490a4d8160a98d9abc3076a720a7c54
14FU <fauzia53.u@gmail.com>
2022-10-20 11:42:00 +0100

    boostarppy

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Hithere from './Props_section/Hithere';
 import data from './Props_section/Data.json';
 import Static from './Props_section/Static';
 import Contact from './routing/contact';
 import Home from './routing/home';
 import User from './routing/Users';
+import OMDB from './routing/omd';
+import Film from './routing/filmie';
 import PageNFound from'./routing/404';
+import {
+  Button, Col, Container, Nav, Row,
+} from 'react-bootstrap';
 //import Products from '../src/lifting_state/Searchlist';
 import {
   BrowserRouter as Router, Link, Route, Routes,
@@ -17,48 +23,65 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>hi there</h1>
-          <body>
-            <h6>i didnt break it </h6>
-            <nav>
-          <Link to="/Home">
-            Home
-          </Link>
-          <p> </p>
-          <Link to="/User">
-            <button type="button">User</button>
-          </Link>
-          <p> </p>
-          <Link to="/Contact">
-            <button type = "buttton">Contact</button>
-          </Link>
-        </nav>
-          </body>
-          <Routes>
-            <Route path = "example" element= {<Hithere/>}/>
-            <Route path = "Static" element= {<Static/>}/>
-            <Route path = "Home" element= {<Home/>}/>
-            <Route path = "User" element= {<User/>}/>
-            <Route path = "Contact" element= {<Contact/>}/>
-            <Route path = "Error Page" element= {<PageNFound/>}/>
-          </Routes>
-          {
-            data.map (s => {return <Static name={s.name} age= {s.age} dream= {s.dream}/>})
-          }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Nav>
+          <Container>
+            <Row>
+              <Col>
+                <Nav.Link href="/home">
+                  Home
+                </Nav.Link>
+              </Col>
+              <Col>
+                <Link to="/PageNFound">
+                  <Button variant="primary" type="button">Erorr Page </Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link className="nav-link" to="/User">
+                  User
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/Hithere">
+                  Hithere
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/Film">
+                  Film
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/filmSearch">
+                  Film Search
+                </Link>
+              </Col>
+              <Col lg={3}>
+              </Col>
+            </Row>
+          </Container>
+
+        </Nav>
+        <br />
+        <header>
+          <h1>I AM A HEADER</h1>
+        </header>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/User" element={<User />} />
+          <Route path="/Hithere" element={<Hithere />} />
+          <Route path="/filmSearch" element={<OMDB />} />
+          <Route path="/Static" element={<Static />} />
+          <Route path="/PageNFound" element={<PageNFound />} />
+          <Route path="/Film" element={<Film />} />
+        </Routes>
+        <footer>
+          I AM A FOOTER
+        </footer>
       </Router>
+      {/* <Converter/> */}
+      {/* <TrueFalse/> */}
     </div>
   );
 }
